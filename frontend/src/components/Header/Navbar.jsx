4d0bd85a7113f8d2cb92869dc76d79c9a4a351a7
Long Le <long.le21062004@gmail.com>
2025-02-04 22:15:40 +0200

    Frontend: Improve navbar css

--- frontend/src/components/Header/Navbar.jsx
+++ frontend/src/components/Header/Navbar.jsx
@@ -1,40 +1,42 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <nav className="navbar">
         <ul className="nav-list">
-          <li className="nav-item">
-            <Link to="/">Home</Link>
+          <li className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
+            <Link to="/" className="nav-link">Home</Link>
           </li>
-          <li className="nav-item">
-            <Link to="/racket">Rackets</Link>
+          <li className={`nav-item ${location.pathname === '/racket' ? 'active' : ''}`}>
+            <Link to="/racket" className="nav-link">Rackets</Link>
           </li>
-          <li className="nav-item">
-            <Link to="/shoes">Shoes</Link>
+          <li className={`nav-item ${location.pathname === '/shoes' ? 'active' : ''}`}>
+            <Link to="/shoes" className="nav-link">Shoes</Link>
           </li>
-          <li className="nav-item">
-            <Link to="/stringings">Stringings</Link>
+          <li className={`nav-item ${location.pathname === '/stringings' ? 'active' : ''}`}>
+            <Link to="/stringings" className="nav-link">Stringings</Link>
           </li>
-          <li className="nav-item">
-            <Link to="/shuttlecocks">Shuttlecocks</Link>
+          <li className={`nav-item ${location.pathname === '/shuttlecocks' ? 'active' : ''}`}>
+            <Link to="/shuttlecocks" className="nav-link">Shuttlecocks</Link>
           </li>
-          <li className="nav-item">
-            <Link to="/grips">Grips</Link>
+          <li className={`nav-item ${location.pathname === '/grips' ? 'active' : ''}`}>
+            <Link to="/grips" className="nav-link">Grips</Link>
           </li>
-          <li className="nav-item">
-            <Link to="/bags">Bags</Link>
+          <li className={`nav-item ${location.pathname === '/bags' ? 'active' : ''}`}>
+            <Link to="/bags" className="nav-link">Bags</Link>
           </li>
         </ul>
-        <div className="auth-links">
+        <ul className="auth-links">
           <Link to="/login">Log In</Link>
           <Link to="/cart">Shopping Bag</Link>
-        </div>
+        </ul>
       </nav>
     </header>
   )
 }
 
-export default Navbar 
+export default Navbar
