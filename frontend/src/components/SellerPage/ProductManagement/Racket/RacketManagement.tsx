f1a216edb2801d40e0cd1437646032ad4ac94839
Long Le <long.le21062004@gmail.com>
2025-03-09 22:23:47 +0200

    Adding delete Racket Form

---
+++ frontend/src/components/SellerPage/ProductManagement/Racket/RacketManagement.tsx
@@ -0,0 +1,26 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { AddRacketForm } from "./AddRacketForm"
+import DeleteRacketForm from "./DeleteRacketForm"
+
+export default function RacketManagement() {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">Racket Management</h1>
+
+      <Tabs defaultValue="add" className="space-y-4">
+        <TabsList>
+          <TabsTrigger value="add">Add Racket</TabsTrigger>
+          <TabsTrigger value="delete">Delete Racket</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="add">
+          <AddRacketForm />
+        </TabsContent>
+
+        <TabsContent value="delete">
+          <DeleteRacketForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
+}
