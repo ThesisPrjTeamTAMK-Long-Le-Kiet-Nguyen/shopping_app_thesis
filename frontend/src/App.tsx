f187817b95b22945c89c1657db1606bbba43dab5
Long Le <long.le21062004@gmail.com>
2025-03-24 12:51:52 +0200

    Adding page footer and fixing typo of confirmation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import ShoppingCart from './components/ShoppingCart/ShoppingCart'
 import ProtectedRoute from './components/ProtectedRoute'
 import { Toaster } from 'sonner'
 import { AuthProvider } from './context/AuthContext'
+import ContactInfo from './components/Footer/ContactInfo'
 import {
   SellerPage,
   RacketManagement,
@@ -26,44 +27,47 @@ const App = () => {
     <AuthProvider>
       <Toaster richColors position="top-center" />
       <Router>
-        <div>
+        <div className="min-h-screen flex flex-col">
           <Navbar />
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/cart" element={<ShoppingCart />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
-            <Route path="/completion" element={<Completion />} />
-            <Route path="/orders" element={<UserOrders />} />
-                        
-            {/* Protected Admin Routes */}
-            <Route path="/seller" element={
-              <ProtectedRoute element={<SellerPage />} allowedRoles={['admin']} />
-            } />
-            <Route path="/seller/orders" element={
-              <ProtectedRoute element={<OrderAdmin />} allowedRoles={['admin']} />
-            } />
-            <Route path="/seller/rackets" element={
-              <ProtectedRoute element={<RacketManagement />} allowedRoles={['admin']} />
-            } />
-            <Route path="/seller/bags" element={
-              <ProtectedRoute element={<BagManagement />} allowedRoles={['admin']} />
-            } />
-            <Route path="/seller/shoes" element={
-              <ProtectedRoute element={<ShoeManagement />} allowedRoles={['admin']} />
-            } />
-            <Route path="/seller/stringings" element={
-              <ProtectedRoute element={<StringingManagement />} allowedRoles={['admin']} />
-            } />
-            <Route path="/seller/grips" element={
-              <ProtectedRoute element={<GripManagement />} allowedRoles={['admin']} />
-            } />
-            <Route path="/seller/shuttlecocks" element={
-              <ProtectedRoute element={<ShuttlecockManagement />} allowedRoles={['admin']} />
-            } />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/cart" element={<ShoppingCart />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/completion" element={<Completion />} />
+              <Route path="/orders" element={<UserOrders />} />
+                          
+              {/* Protected Admin Routes */}
+              <Route path="/seller" element={
+                <ProtectedRoute element={<SellerPage />} allowedRoles={['admin']} />
+              } />
+              <Route path="/seller/orders" element={
+                <ProtectedRoute element={<OrderAdmin />} allowedRoles={['admin']} />
+              } />
+              <Route path="/seller/rackets" element={
+                <ProtectedRoute element={<RacketManagement />} allowedRoles={['admin']} />
+              } />
+              <Route path="/seller/bags" element={
+                <ProtectedRoute element={<BagManagement />} allowedRoles={['admin']} />
+              } />
+              <Route path="/seller/shoes" element={
+                <ProtectedRoute element={<ShoeManagement />} allowedRoles={['admin']} />
+              } />
+              <Route path="/seller/stringings" element={
+                <ProtectedRoute element={<StringingManagement />} allowedRoles={['admin']} />
+              } />
+              <Route path="/seller/grips" element={
+                <ProtectedRoute element={<GripManagement />} allowedRoles={['admin']} />
+              } />
+              <Route path="/seller/shuttlecocks" element={
+                <ProtectedRoute element={<ShuttlecockManagement />} allowedRoles={['admin']} />
+              } />
 
-            <Route path="/*" element={<ProductList />} />
-          </Routes>
+              <Route path="/*" element={<ProductList />} />
+            </Routes>
+          </main>
+          <ContactInfo />
         </div>
       </Router>
     </AuthProvider>
